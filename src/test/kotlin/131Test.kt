330b373c232935e8841a942bb9fec3bba454880e
kirimin <minkarakirimade@gmail.com>
2019-08-11 18:54:53 +0900

    Added abc131-b

---
+++ src/test/kotlin/131Test.kt
@@ -0,0 +1,19 @@
+import abc.problem131b
+import abc.problem137b
+import abc.problem137c
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+class TestOf131 {
+
+    @ParameterizedTest
+    @CsvSource(
+        "5,2,18",
+        "3,-1,0",
+        "30,-50,-1044"
+    )
+    fun testOf_131_b(n: Long, l: Long, expected: Long) {
+        assertEquals(expected, (problem131b(n, l)))
+    }
+}
