da3ae8e5b5d7b83c415143e25bea58e157522b2b
kirimin <minkarakirimade@gmail.com>
2019-09-16 11:40:57 +0900

    added 130-c

--- src/main/kotlin/abc/130-c.kt
+++ src/main/kotlin/abc/130-c.kt
@@ -1,16 +1,18 @@
 package abc
 
-import utilities.debugCount
-import utilities.debugLog
 import java.util.*
 
 fun main(args: Array<String>) {
     val sc = Scanner(System.`in`)
-    val n = sc.nextLong()
-    val a = (0 until n).map { sc.next().toLong() }
-    println(problem130c(n, a))
+    val w = sc.nextLong()
+    val h = sc.nextLong()
+    val x = sc.nextLong()
+    val y = sc.nextLong()
+    println(problem130c(w, h, x, y))
 }
 
-fun problem130c(n: Long, a: List<Long>): Long {
-    return 0L
+fun problem130c(w: Long, h: Long, x: Long, y: Long): String {
+
+    val num = if (x + x == w &&  y + y == h) 1 else 0
+    return "${(w * h / 2.0)} $num"
 }
