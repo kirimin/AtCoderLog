5defb5475c78c70c076f049ef53b1c27e01c556c
kirimin <minkarakirimade@gmail.com>
2019-11-10 19:36:46 +0900

    Added nikkei20192

---
+++ src/main/kotlin/other_contests/nikkei2019/nikkei20192-a.kt
@@ -0,0 +1,40 @@
+package other_contests.nikkei2019
+
+import java.util.*
+
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`)
+    val n = sc.nextInt()
+    println(problemnikkei20192a(n))
+}
+
+fun problemnikkei20192a(n: Int): Int {
+    return n - (n / 2) - 1
+}
+
+// 2
+// 0
+
+// 3
+// 1 + 2
+// 1
+
+// 4
+// 1 + 3
+// 1
+
+// 5
+// 1 + 4, 2 + 3
+// 2
+
+// 6
+// 1 + 4, 2 + 4
+// 2
+
+// 7
+// 1 + 6, 2 + 5, 3 + 4
+// 3
+
+// 8
+// 1 + 7, 2 + 6, 3 + 5
+// 3
