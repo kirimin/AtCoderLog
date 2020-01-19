4cd998221609c640ed198db96240385574f5b99b
kirimin <minkarakirimade@gmail.com>
2020-01-19 16:10:57 +0900

    Added some problems

---
+++ src/main/kotlin/abc/151-d.kt
@@ -0,0 +1,16 @@
+package abc
+
+import utilities.debugCount
+import utilities.debugLog
+import java.util.*
+
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`)
+    val n = sc.nextInt()
+    val a = (0 until n).map { sc.next().toInt() }
+    println(problem151d())
+}
+
+fun problem151d(): Int {
+    return 0
+}
---
+++ src/main/kotlin/other_contests/keyence/2020-c.kt
@@ -0,0 +1,16 @@
+package other_contests.keyence
+
+import utilities.debugCount
+import utilities.debugLog
+import java.util.*
+
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`)
+    val n = sc.nextInt()
+    val a = (0 until n).map { sc.next().toInt() }
+    println(problem2020c())
+}
+
+fun problem2020c(): Int {
+    return 0
+}
